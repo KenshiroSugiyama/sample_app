d70ca18bc7f409338a96aaf17a951f829706e6d2
kenshiro.sugiyama <kenshi.0223@icloud.com>
2019-12-17 01:54:42 +0000

    wip add passwordreset

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,  only:[:new, :create, :edit, :update]
   post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
